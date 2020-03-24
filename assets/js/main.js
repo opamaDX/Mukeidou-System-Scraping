0f59a3f27bfaf826995708c7abf5825cfb2c08d0
nagai <pgtechny@gmail.com>
2020-03-24 20:34:17 +0900

    縦幅を縮小、watch数が0の場合赤色で表示

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 $(function () {
 
+    // watch数が0なら赤色に変更
+    $(".watch").each(function () {
+
+        let index = $(this).attr("data-index");
+        let watch = $(this).text();
+
+        if (watch == 0) {
+            $('[data-index=' + index + '].watch').addClass("text-danger");
+        }
+
+    })
+
+
+
     //テキストボックスのフォーカスが外れたら発火
     $(document).on("blur", "input", function () {
 
@@ -9,16 +23,11 @@ $(function () {
         var buyout_price = Number(start_price) + 2000;
 
         //開始価格の値を入力
-        $('[data-index=' + index + '].start_price').text(start_price);
+        // $('[data-index=' + index + '].start_price').text(start_price);
 
         // 即決価格の値を入力
         $('[data-index=' + index + '].buyout_price').text(buyout_price);
 
-    });
-
-    // ボタンがクリックされると、JSONファイルを出力
-    $("#output_json").click(function () {
-        alert("aaa");
     });
 
 });
