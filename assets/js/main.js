1415c426231ca1b6533b292e41f5a151a086c62d
nagai <pgtechny@gmail.com>
2020-03-31 21:00:53 +0900

    動的に増減率が変わるように変更

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,12 +24,31 @@ $(function () {
         $(this).tooltip('show');
     })
 
-    // 
-    $(document).on("blur","input", function () {
-        alert("a");
+    // フォーカスが外れたとき、増減率を算出
+    $(document).on("blur", "input", function () {
+
+        let index = $(this).attr("data-index");
+        let input_price = $(this).val();
+        let buyout_price = $('[data-index=' + index + '].buyout_price').text();
+
+        // 小数点以下を切り捨てて、増減率を算出
+        range = (input_price / buyout_price - 1) * 100;
+        range = Math.floor(range);
+
+        // 増減率のテキスト表示
+        $('[data-index=' + index + '].range').text(range + "%");
+
+        // 増減率の絶対値が30以上なら、フォームとテキストを赤くする
+        if (Math.abs(range) >= 30) {
+            $('[data-index=' + index + '].range').addClass("text-danger");
+            $('[data-index=' + index + '].input_price').addClass("bg-danger");
+        } else {
+            $('[data-index=' + index + '].range').removeClass("text-danger");
+            $('[data-index=' + index + '].input_price').removeClass("bg-danger");
+        }
     })
 
-    $('#save').on('click', function() {
+    $('#save').on('click', function () {
         alert("クリックされました");
     });
 });
